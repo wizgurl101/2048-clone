bf4c61c7b194ecf0c96e75cdc8664f380ca433f6
wizgurl101 <nb.phan@hotmail.com>
2021-08-26 20:16:49 -0600

    add constants

---
+++ backend/src/constants/gameBoardConstants.ts
@@ -0,0 +1,2 @@
+export const NUMBER_OF_COLUMNS = 4;
+export const NUMBER_OF_ROWS = 4;
---
+++ backend/src/constants/testConstants.ts
@@ -0,0 +1,161 @@
+export const EXPECTED_BOARD = [
+  [
+    { value: 0, beenMerge: false },
+    { value: 0, beenMerge: false },
+    { value: 0, beenMerge: false },
+    { value: 0, beenMerge: false },
+  ],
+  [
+    { value: 0, beenMerge: false },
+    { value: 0, beenMerge: false },
+    { value: 0, beenMerge: false },
+    { value: 0, beenMerge: false },
+  ],
+  [
+    { value: 0, beenMerge: false },
+    { value: 0, beenMerge: false },
+    { value: 0, beenMerge: false },
+    { value: 0, beenMerge: false },
+  ],
+  [
+    { value: 0, beenMerge: false },
+    { value: 0, beenMerge: false },
+    { value: 0, beenMerge: false },
+    { value: 0, beenMerge: false },
+  ],
+];
+
+export const WIN_BOARD = [
+  [
+    { value: 2048, beenMerge: false },
+    { value: 0, beenMerge: false },
+    { value: 0, beenMerge: false },
+    { value: 0, beenMerge: false },
+  ],
+  [
+    { value: 0, beenMerge: false },
+    { value: 0, beenMerge: false },
+    { value: 0, beenMerge: false },
+    { value: 0, beenMerge: false },
+  ],
+  [
+    { value: 0, beenMerge: false },
+    { value: 0, beenMerge: false },
+    { value: 0, beenMerge: false },
+    { value: 0, beenMerge: false },
+  ],
+  [
+    { value: 0, beenMerge: false },
+    { value: 0, beenMerge: false },
+    { value: 0, beenMerge: false },
+    { value: 0, beenMerge: false },
+  ],
+];
+
+export const LOSE_BOARD = [
+  [
+    { value: 4, beenMerge: false },
+    { value: 256, beenMerge: false },
+    { value: 2, beenMerge: false },
+    { value: 8, beenMerge: false },
+  ],
+  [
+    { value: 2, beenMerge: false },
+    { value: 8, beenMerge: false },
+    { value: 32, beenMerge: false },
+    { value: 2, beenMerge: false },
+  ],
+  [
+    { value: 64, beenMerge: false },
+    { value: 2, beenMerge: false },
+    { value: 256, beenMerge: false },
+    { value: 4, beenMerge: false },
+  ],
+  [
+    { value: 4, beenMerge: false },
+    { value: 8, beenMerge: false },
+    { value: 4, beenMerge: false },
+    { value: 2, beenMerge: false },
+  ],
+];
+
+export const ONE_INSERT_BOARD = [
+  [
+    { value: 8, beenMerge: false },
+    { value: 0, beenMerge: false },
+    { value: 16, beenMerge: false },
+    { value: 8, beenMerge: false },
+  ],
+  [
+    { value: 16, beenMerge: false },
+    { value: 256, beenMerge: false },
+    { value: 64, beenMerge: false },
+    { value: 2, beenMerge: false },
+  ],
+  [
+    { value: 4, beenMerge: false },
+    { value: 32, beenMerge: false },
+    { value: 16, beenMerge: false },
+    { value: 2, beenMerge: false },
+  ],
+  [
+    { value: 2, beenMerge: false },
+    { value: 16, beenMerge: false },
+    { value: 8, beenMerge: false },
+    { value: 4, beenMerge: false },
+  ],
+];
+
+export const LEFT_RIGHT_BOARD = [
+  [
+    { value: 2, beenMerge: false },
+    { value: 2, beenMerge: false },
+    { value: 0, beenMerge: false },
+    { value: 4, beenMerge: false },
+  ],
+  [
+    { value: 4, beenMerge: false },
+    { value: 2, beenMerge: false },
+    { value: 4, beenMerge: false },
+    { value: 4, beenMerge: false },
+  ],
+  [
+    { value: 2, beenMerge: false },
+    { value: 2, beenMerge: false },
+    { value: 0, beenMerge: false },
+    { value: 2, beenMerge: false },
+  ],
+  [
+    { value: 0, beenMerge: false },
+    { value: 0, beenMerge: false },
+    { value: 0, beenMerge: false },
+    { value: 0, beenMerge: false },
+  ],
+];
+
+export const UP_DOWN_BOARD = [
+  [
+    { value: 2, beenMerge: false },
+    { value: 2, beenMerge: false },
+    { value: 0, beenMerge: false },
+    { value: 2, beenMerge: false },
+  ],
+  [
+    { value: 4, beenMerge: false },
+    { value: 2, beenMerge: false },
+    { value: 4, beenMerge: false },
+    { value: 4, beenMerge: false },
+  ],
+  [
+    { value: 0, beenMerge: false },
+    { value: 0, beenMerge: false },
+    { value: 0, beenMerge: false },
+    { value: 0, beenMerge: false },
+  ],
+  [
+    { value: 0, beenMerge: false },
+    { value: 0, beenMerge: false },
+    { value: 0, beenMerge: false },
+    { value: 0, beenMerge: false },
+  ],
+];
